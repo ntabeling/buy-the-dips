0890e2adbaa21f65a68cf8c06de70bead8d8c8c3
Nate Tabeling <ntabeling@gmail.com>
2021-09-22 19:35:53 -0400

    update with successful figure generating function

--- testing.py
+++ testing.py
@@ -1,24 +1,29 @@
 import psycopg2
 import os
 from dotenv import load_dotenv
+import pandas as pd
+from sqlalchemy import create_engine
+import plotly.express as px
 
 load_dotenv()
 
-def connect():
-    host='localhost'
-    database=os.getenv('POSTGRES_DB')
-    user=os.getenv('POSTGRES_USER')
-    password=os.getenv('POSTGRES_PASSWORD')
+host='localhost'
+database=os.getenv('POSTGRES_DB')
+user=os.getenv('POSTGRES_USER')
+password=os.getenv('POSTGRES_PASSWORD')
+port = '5432'
 
-    conn = psycopg2.connect(host=host, dbname=database, user=user, password=password)
+def read_records_into_df(table):
+    engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}")
 
-    cur = conn.cursor()
+    df = pd.read_sql_table(table, engine)
 
-    cur.execute('SELECT * from caliber_9mm')
+    return df
 
-    print(cur.fetchone())
-
-    cur.close()
+def make_line_graph(table):
+    df = read_records_into_df(table)
+    fig = px.line(df, x="date", y="cost_per_round", color='casing')
+    fig.show()
 
 if __name__ == '__main__':
-    connect()
+    make_line_graph('caliber_9mm')
